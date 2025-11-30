3410cab506f3353e0f8bb995aec85ef039c64ae9
hunM4P <younghun4218@gmail.com>
2025-11-30 21:45:18 +0900

    랠리 리스트 레이아웃 수정

--- src/components/RallyList.tsx
+++ src/components/RallyList.tsx
@@ -25,7 +25,7 @@ export default function RallyList({ rallies, activeRallyIndex, onRallyClick }: R
     }
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 mt-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-3 mt-4">
             {rallies.map((rally, idx) => (
                 <button
                     key={idx}
